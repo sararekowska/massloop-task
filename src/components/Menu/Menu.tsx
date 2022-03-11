175cd4fc4a88f32ded952434223fb8fee64509cb
sararekowska <sara.rekowska@gmail.com>
2022-03-11 15:54:57 +0100

    feat: mobile main

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,24 @@
+import dev from "../../images/dev.png";
+import "./Menu.scss";
+import { ShopButton } from "../ShopButton/ShopButton";
+
 export const Menu = () => {
-  return <></>;
+  return (
+    <>
+      <section className="main">
+        <h1>Inteligentna kruszarka, która zamienia szkło w piasek.</h1>
+        <img src={dev} alt="kruszarka" />
+        <span className="btn">
+          <ShopButton />
+        </span>
+      </section>
+      <section className="yellow-box">
+        <h2>Płać 5 razy mniej za wywóz odpadów szklanych.</h2>
+        <p>
+          Kruszarka zmniejsza gabaryty szklanych śmieci, a Ty płacisz PIĘ-CIO-KROT-NIE niższe
+          rachunki za ich wywóz.
+        </p>
+      </section>
+    </>
+  );
 };
---
+++ src/components/ShopButton/ShopButton.tsx
@@ -0,0 +1,10 @@
+import vector from "../../images/vector.png";
+import "./ShopButton.scss";
+
+export const ShopButton = () => {
+  return (
+    <div className="button">
+      <img src={vector} alt="shop button" />
+    </div>
+  );
+};
